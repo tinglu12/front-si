a9681dbb49933f797c4be54aa68d7b9fe7c1d762
tinglu <tinglu30@gmail.com>
2025-09-15 14:42:38 -0400

    first push

---
+++ src/features/canvas/components/Whiteboard.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useRef } from "react";
+import { Layer, Line, Stage, Text } from "react-konva";
+import { Button } from "@/components/ui/button";
+
+const Whiteboard = () => {
+  const [tool, setTool] = useState("pen");
+  const [lines, setLines] = useState<any[]>([]);
+  const isDrawing = useRef(false);
+
+  const handleMouseDown = (e: any) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleTrash = () => {
+    setLines([]);
+  };
+
+  const handleMouseMove = (e: any) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+  return (
+    <>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <Button onClick={handleTrash}>Trash</Button>
+
+      <div className="w-full h-full">
+        <Stage
+          width={1000}
+          height={1000}
+          onMouseDown={handleMouseDown}
+          onMousemove={handleMouseMove}
+          onMouseup={handleMouseUp}
+          onTouchStart={handleMouseDown}
+          onTouchMove={handleMouseMove}
+          onTouchEnd={handleMouseUp}
+        >
+          <Layer>
+            <Text text="Hello" />
+            {lines.map((line, i) => (
+              <Line
+                key={i}
+                points={line.points}
+                stroke="#df4b26"
+                strokeWidth={5}
+                tension={0.5}
+                lineCap="round"
+                lineJoin="round"
+                globalCompositeOperation={
+                  line.tool === "eraser" ? "destination-out" : "source-over"
+                }
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+    </>
+  );
+};
+
+export default Whiteboard;
